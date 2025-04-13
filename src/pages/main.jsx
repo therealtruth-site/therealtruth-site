f7e1b1d8ae8d90eb62a8cfb6aa117968ebd1c947
therealtruth-site <systemoverblik@gmail.com>
2025-04-13 13:11:09 -0700

    Update main.jsx

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import HomePage from "../components/HomePage";
 
-export default function Index() {
-  return <HomePage />;
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HomePage />);
