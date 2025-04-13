4c370c1862c15411b5f765c780e5bbfe72930726
therealtruth-site <systemoverblik@gmail.com>
2025-04-13 14:09:50 -0700

    Delete src/src directory

--- src/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HomePage from "./components/HomePage";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<HomePage />);
